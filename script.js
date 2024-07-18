46e7cba0381e2d09ef340504db37f0d871f4a80b
genaaah <genxhatr@hotmail.com>
2024-07-18 03:44:26 -0300

    nuevos cambios

---
+++ script.js
@@ -0,0 +1,9 @@
+//funcion para que la navbar cambie de color nasheee
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
